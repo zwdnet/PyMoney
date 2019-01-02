aabf342f1b3662731ced4dc960b2015c4c81f5cb
zwdnet <zwdnet@163.com>
2019-01-02 23:06:02 +0800

    建立了主程序的框架，再往里面填增删 查改的函数实现就OK啦

---
+++ functions.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+#功能程序:实现程序的具体功能
+
+
+#插入新的收入支出项目
+def InsertData():
+    print("insertdata")
+    input()
+    
+    
+#按项目时间检索数据库
+def SearchByTime():
+    print("SearchByTime")
+    input()
+    
+    
+#按项目类型检索数据库
+def SearchByType():
+    print("SearchByType")
+    input()
+    
+    
+#删除项目
+def DelData():
+    print("删除项目")
+    input()
+    
+    
+#输出现金流量表
+def Statics():
+    print("现金流量表")
+    input()
+    
